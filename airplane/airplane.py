34f6c960ae806836f7ac1341d35b2aad5104af4d
milkmeat <milkmeat@gmail.com>
2015-08-17 21:02:19 +0800

    add gameover

--- airplane/airplane.py
+++ airplane/airplane.py
@@ -3,6 +3,17 @@ from pyglet.window import key
 from cocos.actions import*
 from cocos.director import director
 import random
+import cocos.collision_model as cm
+import cocos.euclid as eu
+
+
+class GameOver(cocos.layer.Layer):
+    def __init__(self):
+        super( GameOver, self ).__init__()
+        
+        background=cocos.sprite.Sprite('resourses/shoot/gameover.png')
+        background.position = 320,240
+        self.add(background)
 
 class HelloWorld(cocos.layer.Layer):
     is_event_handler = True
@@ -72,6 +83,41 @@ class HelloWorld(cocos.layer.Layer):
         
         self.moveBullets()
         self.moveEnemies()
+        self.bump()
+        
+    def bump(self):
+        w,h = director.get_window_size()
+        collman = cm.CollisionManagerGrid(0.0, w,
+                                               0.0, h,
+                                               10,10)
+                                               
+        self.hero.cshape = cm.CircleShape(eu.Vector2(self.hero.x, self.hero.y), 50)     
+        collman.add(self.hero)
+        
+        for one in self.bullets:
+            one.cshape=cm.AARectShape(eu.Vector2(one.x, one.y), 5,10 )
+            collman.add(one)
+        for oneEnemy in self.enemies:
+            oneEnemy.cshape=cm.AARectShape(eu.Vector2(oneEnemy.x, oneEnemy.y), 25,25 )
+            collman.add(oneEnemy)
+        for oneEnemy in self.enemies:
+            for oneBullet in self.bullets:
+                if collman.they_collide(oneEnemy, oneBullet):
+                    self.remove(oneEnemy)
+                    self.enemies.remove(oneEnemy)
+                    self.remove(oneBullet)
+                    self.bullets.remove(oneBullet)
+        for oneEnemy in self.enemies:
+            if collman.they_collide(oneEnemy,self.hero):
+                self.remove(oneEnemy)
+                self.enemies.remove(oneEnemy)
+                #self.remove(hero)
+                #print "you lose!!!!!!!"
+                GameOverLayer=GameOver ()
+                gameover=cocos.scene.Scene (GameOverLayer)
+                director.replace (gameover)
+
+               
     def moveEnemies(self):
         w,h = director.get_window_size()
         for one in self.enemies:
